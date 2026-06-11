72242bef41eabaae9cbceb67f7304aa95b38a344
Delson <69639921+delson-ascent@users.noreply.github.com>
2026-06-11 11:32:02 -0300

    Add configuration for camera streams

---
+++ web/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+  cameras: {
+
+    camera1: {
+      name: "Câmera 1",
+      hls: "http://localhost:8888/camera1/index.m3u8"
+    },
+
+    camera2: {
+      name: "Câmera 2",
+      hls: "http://localhost:8888/camera2/index.m3u8"
+    }
+
+  }
+
+};
