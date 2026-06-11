8c95578c442384197b5c6e7e11b595cce222a190
Delson <69639921+delson-ascent@users.noreply.github.com>
2026-06-11 11:36:46 -0300

    Update camera names and add recordings folders

--- web/config.js
+++ web/config.js
@@ -3,13 +3,15 @@ const CONFIG = {
   cameras: {
 
     camera1: {
-      name: "Câmera 1",
-      hls: "http://localhost:8888/camera1/index.m3u8"
+      name: "Câmera 01",
+      hls: "http://localhost:8888/camera1/index.m3u8",
+      recordingsFolder: "FOLDER01"
     },
 
     camera2: {
-      name: "Câmera 2",
-      hls: "http://localhost:8888/camera2/index.m3u8"
+      name: "Câmera 02",
+      hls: "http://localhost:8888/camera2/index.m3u8",
+      recordingsFolder: "FOLDER02"
     }
 
   }
